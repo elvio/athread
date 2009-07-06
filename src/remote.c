58eea9ccfb106b97359dfc3964a674a921114409
elvio <elviovicosa@elvio-vicosas-macbook.local>
2009-07-06 01:42:11 -0300

    dont know

--- src/remote.c
+++ src/remote.c
@@ -212,7 +212,8 @@ void *athread_remote_slave_execute_job(void *in) {
 	// 
 	// struct remote_service registred_services[100];
 	
-	service = registred_services[service_id];
+	service = &(registred_services[service_id]);
+	printf("found service_id => %d\n", service->service_id);
 	//athread_create(&thread, NULL, service, (void *) input_data);
 	//athread_join(thread, (void *) result_p);
 	
