78ea49ca0c4dab9e5fe80d225a9bbfeed2a10a74
elvio <elviovicosa@elvio-vicosas-macbook.local>
2009-07-11 15:21:25 -0300

    remove mutex

--- src/jobs.c
+++ src/jobs.c
@@ -387,14 +387,12 @@ int athread_join(athread_t id, void **return_data)
 		
 		// Wow.. what about join a remote job Beavis?
 		if (athread_remote_rank == 0 && job->attribs.remote_job) {
-			Pthread_mutex_lock(job->job_list.mutex);
 			result = malloc(sizeof(double));
 			printf("[m] master --- got a join call. Time to request some data hun?\n");
 			*result = request_result_from_slave((job->attribs.remote_job)->slave);
 			printf("[m] master --- got result from slave #%d --- result == %2.2f\n", (job->attribs.remote_job)->slave, *result);
 			mark_slave_as_fresh((job->attribs.remote_job)->slave);
 			job->status = JOB_JOINED;
-			pthread_mutex_unlock(job->job_list.mutex);
 			*return_data = result;
 			return 0;
 		} else if (athread_remote_rank != 0) {
