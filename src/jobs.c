db2b3a7b4f1b7b4f3a3af1e5627f26a1a89fcee7
elvio <elviovicosa@elvio-vicosas-macbook.local>
2009-07-13 05:23:28 -0300

    don't know

--- src/jobs.c
+++ src/jobs.c
@@ -406,12 +406,8 @@ int athread_join(athread_t id, void **return_data)
 			printf("[m] master --- got result from slave #%d --- result == %2.2f\n", (job->attribs.remote_job)->slave, *result);
 			mark_slave_as_fresh((job->attribs.remote_job)->slave);
 			job->status = JOB_JOINED;
-			if (return_data) {
-				*return_data = (void *) result;
-			} else {
-				printf("found return_data == NULL\n");
-			}
-			// *return_data = result;
+			*return_data = (void *) result;
+			printf("return data set --- returning\n");
 			return 0;
 		} else if (athread_remote_rank != 0 && job->attribs.ignore) {
 			printf("[s] slave --- ** ignoring join **\n");
